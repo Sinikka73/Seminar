964445d93e8e52ef04d9b3c26b7d166f3b66c7ef
olga <bend_sinister@mail.ru>
2022-09-23 11:23:14 +0300

    t09 first method

---
+++ Task09/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру в числе
+
+int getRandomNumberFromRange(int minBorder, int maxBorder)
+{
+    int result = new int(); // число, которое будет возвращаться
+    result = newRandom().Next(minBorder, maxBorder + 1); //функция Next возвр.знач. из полуинтервала
+    return result;
+}
+
