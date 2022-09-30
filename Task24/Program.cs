0fc5e50c4034983452f783ad99322cee092b1bb1
olga <bend_sinister@mail.ru>
2022-09-30 10:34:11 +0300

    T24

---
+++ Task24/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход 
+//число (А) и выдаёт сумму чисел от 1 до А.
+
+int getSumOfRange(int startPoint, int endPoint)
+{
+    int result = 0;
+    //endPoint = Math.Abs(endPoint); //ф-ция модуля числа
+    if(startPoint > endPoint)
+    {
+        //Console.ForegraundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"Верхняя граница отрицательная {endPoint}");
+        //Console.ResetColor();
+        int buff = startPoint;
+        startPoint = endPoint;
+        endPoint = buff;
+    }
+    for(int i = startPoint; i <= endPoint; i++)
+    {
+        result += i;//result = result + i
+    }
+    return result;
+}
+
+Console.WriteLine("Введите верхнюю границу для суммы ряда");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int sumOfRange = getSumOfRange(1, userNumber);
+Console.WriteLine($"Сумма всех чисел от 1 до {userNumber} равна {sumOfRange}");
