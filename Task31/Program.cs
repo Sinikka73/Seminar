5565b3ca870d469b809a6c1ce8e0c340a787e111
olga <bend_sinister@mail.ru>
2022-10-03 16:52:16 +0300

    T31 T23

---
+++ Task31/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами 
+//из промежутка [-9, 9]. Найдите сумму отрицательных и положительных
+// элементов массива.
+//Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма 
+//положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for(int i =0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write ("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if(i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int getSumOfPositiveNumbersInArray(int[] incomingArray)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+       if(incomingArray[i] > 0)
+       {
+        result = result + incomingArray[i];
+       } 
+    }
+    return result;
+}
+
+int getSumOfNegativeNumbersInArray(int[] incomingArray)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+       if(incomingArray[i] < 0)
+       {
+        result = result + incomingArray[i];
+       } 
+    }
+    return result;
+}
+
+// deviation =1, если считать положит. И -1, если отриц.
+int getSumOfNegativeOrePositiveNumbersInArray(int[] incomingArray, int deviation)
+{
+    int result = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+       if(incomingArray[i] * deviation > 0)
+       {
+        result = result + incomingArray[i];
+       } 
+    }
+    return result;
+}
+
+int[] currentArray = getRandomArray(12, -9, 9);
+printArray(currentArray);
+
+int sumOfPositive = getSumOfPositiveNumbersInArray(currentArray);
+int sumOfNegative = getSumOfNegativeNumbersInArray(currentArray);
+Console.WriteLine($"Сумма положительных элементов равна {sumOfPositive}. Сумма отрицательных элементов равна {sumOfNegative}");
+
+sumOfPositive = getSumOfNegativeOrePositiveNumbersInArray(currentArray, 1);
+sumOfNegative = getSumOfNegativeOrePositiveNumbersInArray(currentArray, -1);
+Console.WriteLine($"Сумма положительных элементов равна {sumOfPositive}. Сумма отрицательных элементов равна {sumOfNegative}");
---
+++ Task32/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for(int i =0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write ("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if(i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+void printReversArray(int[] incomingArray)
+{
+    Console.Write ("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        incomingArray[i] = incomingArray[i] * (-1);
+        Console.Write(incomingArray[i]);
+        if(i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+
+int[] currentArray = getRandomArray(12, -9, 9);
+Console.WriteLine("Исходный массив: ");
+printArray(currentArray);
+Console.WriteLine("Массив с изменёнными на противоположные знаки элементов: ");
+printReversArray(currentArray);
